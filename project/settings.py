826e2ade2405d308a5bc2e539759d6969dc72876
Llona <liu.lon@gmail.com>
2017-08-21 10:28:13 +0800

    try to modify media path to dropbox

--- project/settings.py
+++ project/settings.py
@@ -137,7 +137,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+DROPBOX_OAUTH2_TOKEN = 'QxsccMIJLdAAAAAAAAAAB0IxRBQfbLhQmgBsr4wLVFCqXuRIzItyWwh6ubZXRhm2'
+DROPBOX_ROOT_PATH = 'ajmywatch'
+MEDIA_URL = r'http://https://www.dropbox.com/home/'
+MEDIA_ROOT = MEDIA_URL
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
